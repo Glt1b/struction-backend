367cdf1b7aebefd206a4f82013c75570f4641b20
aelorn13 <a.maslov364@gmail.com>
2022-12-21 10:01:44 +0000

    allow cross origin resource sharing

--- app.js
+++ app.js
@@ -1,22 +1,25 @@
-const express = require('express');
+const express = require("express");
 
-
-const { getUser, getProject, postMarker, delMarker } = require('./controllers/controllers.js');
-
+const {
+  getUser,
+  getProject,
+  postMarker,
+  delMarker,
+} = require("./controllers/controllers.js");
+const cors = require("cors");
 const app = express();
+app.use(cors());
 app.use(express.json());
 
-app.get('/api/users/:username', getUser);
-app.get('/api/projects/:project_name', getProject);
+app.get("/api/users/:username", getUser);
+app.get("/api/projects/:project_name", getProject);
 
-app.post('/api/markers/:project_name', postMarker);
+app.post("/api/markers/:project_name", postMarker);
 
-app.delete('/api/:project_name/:marker_id', delMarker);
+app.delete("/api/:project_name/:marker_id", delMarker);
 
-
-app.all('/*', (req, res) => {
-    res.status(404).send({ msg: 'Path not found' });
-  });
-  
+app.all("/*", (req, res) => {
+  res.status(404).send({ msg: "Path not found" });
+});
 
 module.exports = app;
