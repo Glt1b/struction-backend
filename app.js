56f363d3400af8bd3867797559218d8d8f6a2824
MARCIN <m.palenik2015@gmail.com>
2023-01-13 09:57:07 +0000

    patchs changed

--- app.js
+++ app.js
@@ -18,17 +18,10 @@ app.use(express.json());
 
 app.use(function (req, res, next) {
 
-  // Website you wish to allow to connect
+
   res.setHeader('Access-Control-Allow-Origin', '*');
-
-  // Request methods you wish to allow
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-  // Request headers you wish to allow
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
   res.setHeader('Access-Control-Allow-Credentials', true);
 
   // Pass to next layer of middleware
@@ -43,7 +36,7 @@ app.post("/api/markers/:project_name", postMarker);
 
 app.delete("/api/markers/:project_name/:marker_id", delMarker);
 
-app.patch("/api/:project_name/:marker_id", patchMarker);
+app.patch("/api/markers/:project_name/:marker_id", patchMarker);
 
 // images
 app.post("/api/image/:image_id", postImage);
