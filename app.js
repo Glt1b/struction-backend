51b49c79350e2487edc96dc39885435c143ebfed
MARCIN <m.palenik2015@gmail.com>
2023-01-19 09:16:53 +0000

    cors7

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(express.json({limit: '5mb'}));
  /* */
 app.use(function (req, res, next) {
 
-  res.setHeader('Access-Control-Allow-Origin', *);
+  res.setHeader('Access-Control-Allow-Origin', '*');
 
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
