9fc673c39789fc78cc0f3b1bc153022329331074
MARCIN <m.palenik2015@gmail.com>
2023-03-13 20:18:04 +0000

    exceed limit

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const cors = require("cors");
 const app = express();
 
 app.use(cors({origin: '*'}));
-app.use(express.json({limit: '5mb'}));
+app.use(express.json({limit: '10mb'}));
 // app.use(bodyParser.json({limit: '5mb'}));
 
  /* 
