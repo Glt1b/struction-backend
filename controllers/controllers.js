32cf2302149d0b975d9bf448d9c31e2eb13531fc
MARCIN <m.palenik2015@gmail.com>
2023-03-16 21:33:16 +0000

    3

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -62,6 +62,8 @@ exports.getImage = (req, res) => {
 
 exports.postImage = (req, res) => {
 
+    console.log(req)
+
     postImageS3(req.params.image_id, req.body.data).then((result) => {
         res.status(201).send()
     })
