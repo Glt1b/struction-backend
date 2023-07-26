4ac04d0988ec2dcf42fa4a2b144abc522b8903a5
MARCIN <m.palenik2015@gmail.com>
2023-07-26 09:57:26 +0100

    photo issue

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -122,7 +122,7 @@ exports.getImage = (req, res) => {
 exports.postImage = (req, res) => {
 
     postImageS3(req.params.image_id, req.body.data).then((result) => {
-        res.status(201).send()
+        res.status(201).send(req.body.data)
     })
 }
 
